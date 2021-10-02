f055f91efa5c8fc18e8b21685b126efaf889901a
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-02 14:10:53 +0100

    ams version 1 (spring boot) - MVC

---
+++ src/main/java/com/sip/ams/controllers/EtudiantController.java
@@ -0,0 +1,18 @@
+package com.sip.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EtudiantController {
+	
+	@RequestMapping("/home")
+	public String message(Model model)
+	{
+		System.out.println("Bienvenue au BootCamp");
+		String formation = "fullstack";
+		return "info";
+	}
+
+}
