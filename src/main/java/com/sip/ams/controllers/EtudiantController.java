443bf7bf05fc7819bb5cba3dde12fcc4c689c9d5
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-02 16:21:39 +0100

    exo 2

--- src/main/java/com/sip/ams/controllers/EtudiantController.java
+++ src/main/java/com/sip/ams/controllers/EtudiantController.java
@@ -2,58 +2,78 @@ package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sip.ams.entities.Etudiant;
 
 import java.util.*;
+
 @Controller
 public class EtudiantController {
+	List<Etudiant> etudiants = new ArrayList<>();
 	
-	@RequestMapping("/home")
-	public String message(Model model)
 	{
+		
+		etudiants.add(new Etudiant(1, "amine", "amine@gmail.com"));
+		etudiants.add(new Etudiant(2, "rami", "rami@gmail.com"));
+		etudiants.add(new Etudiant(3, "patrick", "patrick@gmail.com"));
+	}
+
+	@RequestMapping("/home")
+	public String message(Model model) {
 		System.out.println("Bienvenue au BootCamp");
 		String formation = "fullstack 100% Spring boot";
-		String lieu ="Sesame";
+		String lieu = "Sesame";
 		model.addAttribute("training", formation);
 		model.addAttribute("location", lieu);
 		return "info";
 	}
-	
+/*
 	@RequestMapping("/produits")
-	//public String listProduits(Model model)
-	public ModelAndView listProduits()
-	{
+	// public String listProduits(Model model)
+	public ModelAndView listProduits() {
 		ModelAndView mv = new ModelAndView();
-		
-		List<String> produits = new ArrayList<>();
-		produits.add("Voiture");
-		produits.add("Camion");
-		produits.add("Moto");
-		produits.add("Bus");
-		//model.addAttribute("mesProduits", produits);
+
+		// model.addAttribute("mesProduits", produits);
 		mv.addObject("mesProduits", produits);
 		mv.setViewName("products");
 		return mv;
-		//return "products";
-	}
-	
+		// return "products";
+	}*/
+
 	@RequestMapping("/students")
-	public ModelAndView listStudents()
-	{
+	public ModelAndView listStudents() {
 		ModelAndView mv = new ModelAndView();
-		
-		List<Etudiant> etudiants = new ArrayList<>();
-		
-		etudiants.add(new Etudiant(1,"amine","amine@gmail.com"));
-		etudiants.add(new Etudiant(2,"rami","rami@gmail.com"));
-		etudiants.add(new Etudiant(3,"patrick","patrick@gmail.com"));
-        System.out.println(etudiants);
+
+		System.out.println(this.etudiants);
 		mv.addObject("students", etudiants);
 		mv.setViewName("listStudents");
 		return mv;
+	}
+
+	// @GetMapping("/add")
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public ModelAndView addStudentForm() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("addStudent");
+		return mv;
+
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addStudent(@RequestParam("id") int id, @RequestParam("nomEtudiant") String nom,
+			@RequestParam("email") String email) {
+		// ModelAndView mv = new ModelAndView();
+		// mv.setViewName("listStudents");
+		// return mv;
+		Etudiant e = new Etudiant(id, nom, email);
+		etudiants.add(e);
+		return "redirect:students";
+
 	}
 
 }
