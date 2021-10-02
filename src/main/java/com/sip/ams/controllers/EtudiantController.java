057e342931b6c5a3103e87ddb4a9c5b8af1d4110
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-02 15:32:05 +0100

    exo 2 part1

--- src/main/java/com/sip/ams/controllers/EtudiantController.java
+++ src/main/java/com/sip/ams/controllers/EtudiantController.java
@@ -3,6 +3,10 @@ package com.sip.ams.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sip.ams.entities.Etudiant;
+
 import java.util.*;
 @Controller
 public class EtudiantController {
@@ -19,15 +23,37 @@ public class EtudiantController {
 	}
 	
 	@RequestMapping("/produits")
-	public String listProduits(Model model)
+	//public String listProduits(Model model)
+	public ModelAndView listProduits()
 	{
+		ModelAndView mv = new ModelAndView();
+		
 		List<String> produits = new ArrayList<>();
 		produits.add("Voiture");
 		produits.add("Camion");
 		produits.add("Moto");
 		produits.add("Bus");
-		model.addAttribute("mesProduits", produits);
-		return "products";
+		//model.addAttribute("mesProduits", produits);
+		mv.addObject("mesProduits", produits);
+		mv.setViewName("products");
+		return mv;
+		//return "products";
+	}
+	
+	@RequestMapping("/students")
+	public ModelAndView listStudents()
+	{
+		ModelAndView mv = new ModelAndView();
+		
+		List<Etudiant> etudiants = new ArrayList<>();
+		
+		etudiants.add(new Etudiant(1,"amine","amine@gmail.com"));
+		etudiants.add(new Etudiant(2,"rami","rami@gmail.com"));
+		etudiants.add(new Etudiant(3,"patrick","patrick@gmail.com"));
+        System.out.println(etudiants);
+		mv.addObject("students", etudiants);
+		mv.setViewName("listStudents");
+		return mv;
 	}
 
 }
