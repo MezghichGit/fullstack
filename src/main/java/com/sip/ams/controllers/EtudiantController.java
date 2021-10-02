7c8174c4214d84067eea9eaed359d3e819441003
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2021-10-02 14:52:06 +0100

    exo1

--- src/main/java/com/sip/ams/controllers/EtudiantController.java
+++ src/main/java/com/sip/ams/controllers/EtudiantController.java
@@ -3,7 +3,7 @@ package com.sip.ams.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import java.util.*;
 @Controller
 public class EtudiantController {
 	
@@ -16,6 +16,18 @@ public class EtudiantController {
 		model.addAttribute("training", formation);
 		model.addAttribute("location", lieu);
 		return "info";
+	}
+	
+	@RequestMapping("/produits")
+	public String listProduits(Model model)
+	{
+		List<String> produits = new ArrayList<>();
+		produits.add("Voiture");
+		produits.add("Camion");
+		produits.add("Moto");
+		produits.add("Bus");
+		model.addAttribute("mesProduits", produits);
+		return "products";
 	}
 
 }
